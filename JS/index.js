1f7cc906adc824741f74ab1a3d9bdf590dbdeb98
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-09-02 23:13:31 +0600

    navbar and category section done

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,28 @@
-console.log('connected');
+const loadCategories = async() =>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        // console.log(data.data.news_category);
+        return data.data.news_category;
+    } catch (error) {
+        
+    }
+}
+
+const displayCategories = async() =>{
+    const categories = await loadCategories();
+    // console.log(categories);
+    const categoryContainer = document.getElementById('category-container');
+    categories.forEach(category => {
+        // console.log(category);
+        const categoryDiv = document.createElement('div');
+        categoryDiv.innerHTML = `
+            <p>${category.category_name}</p>
+        `;
+        categoryContainer.appendChild(categoryDiv);
+    });
+}
+
+displayCategories();
+// loadCategories();
