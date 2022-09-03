4a71342375a94f24462e392845e863c941b81c1e
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-09-03 12:33:26 +0600

    modal added

--- JS/index.js
+++ JS/index.js
@@ -77,8 +77,8 @@ const displayCategoryItems = (items) => {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /></svg></p>
                         <p>${item.total_view ? item.total_view : "No View"}</p>
                     </div>
-                    <button onclick="loadItemDetails('${item._id}')" class="btn bg-inherit border-none"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" /></svg></button>
+                    <label onclick="loadItemDetails('${item._id}')" for="my-modal-3" class="btn modal-button bg-inherit border-none"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" /></svg></label>
                 </div>
             </div>
         </div>
@@ -97,7 +97,20 @@ const loadItemDetails = (itemId) => {
     .then(data => displayItemDetailsInModal(data.data[0]))
 }
 const displayItemDetailsInModal = (newsDetail) => {
-    console.log(newsDetail);
+    // console.log(newsDetail);
+    const modalContainer = document.getElementById('modal-container');
+    const modalDiv = document.createElement('div');
+    modalDiv.innerHTML = `
+        <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+        <div class="modal">
+        <div class="modal-box relative">
+            <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+            <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
+            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+        </div>
+        </div>
+    `;
+    modalContainer.appendChild(modalDiv);
     
 }
 // displayCategoryItems()
